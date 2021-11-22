ef464ecb69d774922bd435ea5c72bdbf95649a10
Peter Jensen <peter.jensen@cellari.io>
2021-11-22 15:18:40 +0100

    refactor(adding-code-quality): adding code quality and refactoring camera_settings
    rest of the project is still missing refactoring

--- unforeseen/cameras/camera_settings.py
+++ unforeseen/cameras/camera_settings.py
@@ -1,11 +1,10 @@
 # Settings can be set with
 # power_line_frequency = 1 for Europe (50 hz) and 2 for us / japan (60 hz)
-# if the settings has a flags=inactive it will be invalid or incomplete, we can ignore this.
-# set exposure_auto to 1 in order to not screw with the framerate while recording
-import sys, os
-import json
+# if the settings has a flags=inactive it will be invalid or incomplete.
+# set exposure_auto to 1 in order to not screw with
+# the framerate while recording
 import subprocess
-import pdb
+
 
 def apply_camera_settings(camera):
     camera_settings = {}
@@ -14,9 +13,12 @@ def apply_camera_settings(camera):
         print(f)
         for line in f:
             print(line)
-            camera_settings[line.lstrip().split(" ")[0]]=int(line.lstrip().split("value=")[-1].split(" ")[0])
+            camera_settings[line.lstrip().split(" ")[0]] = int(
+                line.lstrip().split("value=")[-1].split(" ")[0]
+            )
 
     for key in camera_settings:
-        subprocess.call([f"v4l2-ctl -d {camera} --set-ctrl {key}={camera_settings[key]}"], shell=True)
-        
-#apply_camera_settings("/dev/video0")
+        subprocess.call(
+            [f"v4l2-ctl -d {camera} --set-ctrl {key}={camera_settings[key]}"],
+            shell=True,
+        )
