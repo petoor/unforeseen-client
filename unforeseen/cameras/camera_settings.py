929ac505f58a57b676268ab794f801dabd9a6880
Peter Jensen <peter.jensen@cellari.io>
2021-11-23 09:24:48 +0100

    added pre-commit hooks

--- unforeseen/cameras/camera_settings.py
+++ unforeseen/cameras/camera_settings.py
@@ -1,21 +1,17 @@
-# Settings can be set with
-# power_line_frequency = 1 for Europe (50 hz) and 2 for us / japan (60 hz)
-# if the settings has a flags=inactive it will be invalid or incomplete.
-# set exposure_auto to 1 in order to not screw with
-# the framerate while recording
 import subprocess
 
 
-def apply_camera_settings(camera):
+def apply_camera_settings(camera: str) -> None:
+    """Applies the specified settings to the camera.
+
+    Args:
+        camera (str): [description]
+    """
     camera_settings = {}
 
     with open(f"cameras/{camera.split('/')[-1]}.txt", "r") as f:
-        print(f)
         for line in f:
-            print(line)
-            camera_settings[line.lstrip().split(" ")[0]] = int(
-                line.lstrip().split("value=")[-1].split(" ")[0]
-            )
+            camera_settings[line.lstrip().split(" ")[0]] = int(line.lstrip().split("value=")[-1].split(" ")[0])
 
     for key in camera_settings:
         subprocess.call(
