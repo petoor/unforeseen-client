9e9d1c70c909fe2b0ffbd2f2193fae455b137735
Peter Jensen <peter.jensen@cellari.io>
2022-01-22 12:18:02 +0100

    added correct log path and added -OO arg

--- unforeseen/system/start_cronjobs.py
+++ unforeseen/system/start_cronjobs.py
@@ -13,18 +13,18 @@ cron.remove_all()
 
 # Sync and remove
 sync_and_remove = cron.new(
-    command=f"cd {root} && python3 system/scripts/sync_remove.py > storage/logging/sync_and_remove.log"
+    command=f"cd {root} && python -OO system/scripts/sync_remove.py >> {root}/storage/logging/sync_and_remove.log"
 )
 sync_and_remove.minute.on(0)
 
 # Backup settings
-backup = cron.new(command=f"cd {root} && python3 system/scripts/backup_settings.py > storage/logging/backup.log")
+backup = cron.new(command=f"cd {root} && python -OO system/scripts/backup_settings.py >> {root}/storage/logging/backup.log")
 backup.minute.on(0)
 backup.hour.on(0)
 
 # Start http (hls) server if machine goes down
 hls_sink_port = setup.get("server").get("hls_sink_port")  # type: ignore
-server = cron.new(command=f"cd {root} && python3 -m http.server {hls_sink_port} > storage/logging/server.log")
+server = cron.new(command=f"cd {root} && python -m http.server {hls_sink_port} >> {root}/storage/logging/server.log")
 server.every_reboot()
 
 # Start analysis
@@ -34,6 +34,6 @@ camera = setup.get("model").get("camera")  # type: ignore
 use_db = setup.get("model").get("use_db")  # type: ignore
 use_gpio = setup.get("model").get("use_gpio")  # type: ignore
 use_model = setup.get("model").get("use_model")  # type: ignore
-analysis = cron.new(command=f"cd {root} && python3 {script} --pipeline {pipeline} --camera {camera} --use_gpio {use_gpio} --use_db {use_db} --use_model {use_model} > storage/logging/analysis.log")  # type: ignore # noqa
+analysis = cron.new(command=f"cd {root} && python -OO {script} --pipeline {pipeline} --camera {camera} --use_gpio {use_gpio} --use_db {use_db} --use_model {use_model} >> {root}/storage/logging/analysis.log")  # type: ignore # noqa
 analysis.every_reboot()
 cron.write()
